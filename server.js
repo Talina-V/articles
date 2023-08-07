63d5208557c203dc4901bf0bc27c33781bcb7eed
Talina <tshabliy@gmail.com>
2023-08-08 00:40:11 +0300

    added server

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
-//Install express server
-// const express = require('express');
-// const path = require('path');
-//
-// const app = express();
-//
-// app.use(express.static('./dist/heroku'));
-//
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-// );
-//
-// app.listen(process.env.PORT || 8080);
-
-
 const express = require('express');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/articles'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/articles/index.html'));
-});
 
-app.listen(process.env.PORT || 8080);
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/herokuubsmi'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
