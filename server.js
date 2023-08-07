a95bd6ef6dd8eb4470aad5dbf9e9cce6be428b76
Talina <tshabliy@gmail.com>
2023-08-08 00:57:03 +0300

    added minor fixes

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/herokuubsmi'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
+app.use(express.static(__dirname + '/dist/articles'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/articles/index.html'));
+});
